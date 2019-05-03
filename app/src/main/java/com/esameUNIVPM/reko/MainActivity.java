032044d28122a4d9a4c18df329e52c354987c7b0
alegigli97 <50239217+alegigli97@users.noreply.github.com>
2019-05-03 17:53:33 +0200

    add processing
    Add processing control class and internet permission in android manifest.

--- app/src/main/java/com/esameUNIVPM/reko/MainActivity.java
+++ app/src/main/java/com/esameUNIVPM/reko/MainActivity.java
@@ -2,12 +2,24 @@ package com.esameUNIVPM.reko;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import processing.android.CompatUtils;
+import processing.android.PFragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    MainProcessing mainprocessing;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        FrameLayout frame = new FrameLayout(this);
+        frame.setId(CompatUtils.getUniqueViewId());
+        setContentView(frame, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+
+        mainprocessing = new MainProcessing();
+        PFragment fragment = new PFragment(mainprocessing);
+        fragment.setView(frame, this);
     }
 }
---
+++ app/src/main/java/com/esameUNIVPM/reko/MainProcessing.java
@@ -0,0 +1,15 @@
+package com.esameUNIVPM.reko;
+
+import processing.core.PApplet;
+
+public class MainProcessing extends PApplet {
+    MainProcessing (){}
+    public void setup(){}
+    public void settings(){
+        size(width,height);
+    }
+    public void draw (){
+        background(255);
+    }
+
+}
