94e94d3e0e83e9cffbd4fad4413e4ab59c9c4f66
tommaso2212 <t.pavani97@gmail.com>
2019-05-06 21:12:00 +0200

    Add interface for pick image

---
+++ app/src/main/java/com/esameUNIVPM/reko/PickImage.java
@@ -0,0 +1,11 @@
+package com.esameUNIVPM.reko;
+
+import java.nio.ByteBuffer;
+
+public interface PickImage {
+
+    void selectImage();
+
+    ByteBuffer getImage();
+
+}
---
+++ app/src/main/java/com/esameUNIVPM/reko/PickImageCamera.java
@@ -0,0 +1,14 @@
+package com.esameUNIVPM.reko;
+
+import java.nio.ByteBuffer;
+
+public class PickImageCamera implements PickImage{
+
+    public void selectImage() {
+        
+    }
+
+    public ByteBuffer getImage() {
+        return null;
+    }
+}
