9e21eec773d0890c97a4acfe554cfb2c9f9ab4a0
alegigli97 <alessandrogigli973@gmail.com>
2019-05-06 12:44:19 +0200

    new camerabutton animation,

--- app/src/main/java/com/esameUNIVPM/reko/CameraButton.java
+++ app/src/main/java/com/esameUNIVPM/reko/CameraButton.java
@@ -16,10 +16,28 @@ public class CameraButton extends MainButton {
     }
     @Override
     public void draw(){
+        p.fill(p.color(255,0,0));
+        p.ellipse(p.width-200, p.height-200,200,200);
+        p.image(camerabutton, p.width-300, p.height-300, 200, 200);
+        animation();
 
     }
     @Override
     public void animation(){
+        if(p.mousePressed){
+            if(p.mouseX>p.width-300 && p.mouseX<p.width-100){
+                if(p.mouseY>p.height-300 && p.mouseY<p.height-100){
+                    for(int i=0; i<200; i=i+3){
+                        p.noFill();
+                        p.stroke(p.color(160,0,0));
+                        p.ellipse(p.width-200, p.height-200,i,i);
+
+
+                    }
+                }
+
+            }
+        }
 
     }
     @Override
--- app/src/main/java/com/esameUNIVPM/reko/MainProcessing.java
+++ app/src/main/java/com/esameUNIVPM/reko/MainProcessing.java
@@ -3,13 +3,17 @@ package com.esameUNIVPM.reko;
 import processing.core.PApplet;
 
 public class MainProcessing extends PApplet {
+    private CameraButton cameraButton;
     MainProcessing (){}
-    public void setup(){}
+    public void setup(){
+        cameraButton = new CameraButton(this);
+    }
     public void settings(){
         size(width,height);
     }
     public void draw (){
         background(255);
+        cameraButton.draw();
     }
 
 }
